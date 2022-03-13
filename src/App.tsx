ad2a41371522547169c79bf8532c867b51b3ae57
IshaBP <isha.pachkawade11@gmail.com>
2022-03-13 15:56:20 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <p>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default App;
