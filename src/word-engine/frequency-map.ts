8c74ef556fa396d5cd352e37e99749bd4e91c9bc
IshaBP <isha.pachkawade11@gmail.com>
2022-03-15 22:42:21 +0530

    frequency map TDD

--- src/word-engine/frequency-map.ts
+++ src/word-engine/frequency-map.ts
@@ -1,5 +1,5 @@
 export class FrequencyMap {
-  map = new Map();
+  #map = new Map<string, number>();
 
   constructor(word: string) {
     for (let idx = 0; idx < word.length; idx++) {
@@ -8,24 +8,24 @@ export class FrequencyMap {
   }
 
   has(letter: string): boolean {
-    return this.map.has(letter) && this.map.get(letter) !== 0;
+    return this.#map.has(letter) && this.get(letter) !== 0;
   }
 
   get(letter: string): number {
-    return this.map.get(letter) ?? 0;
+    return this.#map.get(letter) ?? 0;
   }
 
   increment(letter: string): void {
     if (this.has(letter)) {
-      this.map.set(letter, this.map.get(letter) + 1);
+      this.#map.set(letter, this.get(letter) + 1);
     } else {
-      this.map.set(letter, 1);
+      this.#map.set(letter, 1);
     }
   }
 
   decrement(letter: string): void {
     if (this.has(letter)) {
-      this.map.set(letter, this.map.get(letter) - 1);
+      this.#map.set(letter, this.get(letter) - 1);
     }
   }
 }
