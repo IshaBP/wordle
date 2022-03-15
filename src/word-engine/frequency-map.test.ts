c836f938bc9fd4c7f0a51df0e7f70e7298528c7a
IshaBP <isha.pachkawade11@gmail.com>
2022-03-15 22:29:48 +0530

    frequency map TDD

--- src/word-engine/frequency-map.test.ts
+++ src/word-engine/frequency-map.test.ts
@@ -1,15 +1,42 @@
 import { FrequencyMap } from "./frequency-map";
 
 describe("Frequency map", () => {
-  it.todo("should initialize frequency map with given word");
+  it("should initialize frequency map with given word", () => {
+    const freqMap = new FrequencyMap("mamma");
 
-  it.todo("should be able to decrement frequency of given letter");
+    expect(freqMap.get("m")).toEqual(3);
+    expect(freqMap.get("a")).toEqual(2);
+  });
 
-  it.todo("should not decrement frequency of given letter if it is already 0");
+  it("should be able to decrement frequency of given letter", () => {
+    const freqMap = new FrequencyMap("mamma");
 
-  it.todo(
-    "should not decrement frequency of given letter if it is not present"
-  );
+    freqMap.decrement("m");
+    expect(freqMap.get("m")).toEqual(2);
+
+    freqMap.decrement("a");
+    expect(freqMap.get("a")).toEqual(1);
+  });
+
+  it("should not decrement frequency of given letter if it is already 0", () => {
+    const freqMap = new FrequencyMap("mamma");
+
+    freqMap.decrement("m");
+    freqMap.decrement("m");
+    freqMap.decrement("m");
+    freqMap.decrement("m");
+    freqMap.decrement("m");
+
+    expect(freqMap.get("m")).toEqual(0);
+  });
+
+  it("should not decrement frequency of given letter if it is not present", () => {
+    const freqMap = new FrequencyMap("mamma");
+
+    expect(freqMap.get("b")).toEqual(0);
+    freqMap.decrement("b");
+    expect(freqMap.get("b")).toEqual(0);
+  });
 
   it.todo("should be able to increment frequency of given letter");
 
