77ab5831a123141ffa95d669f5b5ebb63e8e71da
IshaBP <isha.pachkawade11@gmail.com>
2022-03-17 11:47:22 +0530

    Keyboard tests TDD

---
+++ src/components/Keyboard.test.tsx
@@ -0,0 +1,16 @@
+import { Keyboard } from "./Keyboard";
+
+describe("Keyboard", () => {
+  it.todo("should display all alphabets");
+  it.todo("should display enter and backspace keys");
+  it.todo("should display all keys in QWERTY order");
+  it.todo(
+    "should trigger onKey callback on clicking alphabet, <ENT>, <BKSP> buttons on display"
+  );
+  it.todo(
+    "should trigger onKey callback on typing alphabet, <ENT>, <BKSP> from physical keyboard"
+  );
+  it.todo(
+    "should not trigger onKey callback on typing any keys other than alphabets, <ENT>, <BKSP>"
+  );
+});
