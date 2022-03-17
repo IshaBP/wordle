d516448f77d83b0f54e9fff3ec1e898ae6ada1a9
IshaBP <isha.pachkawade11@gmail.com>
2022-03-17 18:45:52 +0530

    <WordBoard /> skeleton TDD

--- src/components/wordboard/Wordboard.test.tsx
+++ src/components/wordboard/Wordboard.test.tsx
@@ -1,8 +1,21 @@
-import { Wordboard } from "./Wordboard";
+import { getAllByLabelText, render, screen } from "@testing-library/react";
+import { Wordboard, WordboardProps } from "./Wordboard";
 
 describe("Wordboard", () => {
-  it.todo("should have 6 guess words");
-  it.todo("should have 5 letter tiles in each guess word");
+  it("should have 6 guess words", () => {
+    render(<Wordboard game={game} latestRowStatus={"IN_PROGRESS"} />);
+
+    expect(screen.getAllByLabelText("guess-word")).toHaveLength(6);
+  });
+
+  it("should have 5 letter tiles in each guess word", () => {
+    render(<Wordboard game={game} latestRowStatus={"IN_PROGRESS"} />);
+
+    for (let guessWord of screen.getAllByLabelText("guess-word")) {
+      expect(getAllByLabelText(guessWord, "letter")).toHaveLength(5);
+    }
+  });
+
   it.todo("should colour tile with black if MATCH_STATUS is 'INITIAL'");
   it.todo("should colour tile with green if MATCH_STATUS is 'MATCH'");
   it.todo("should colour tile with yellow if MATCH_STATUS is 'PARTIAL_MATCH'");
@@ -11,9 +24,19 @@ describe("Wordboard", () => {
 
   // Move to toast
   it.todo(
-    "should not prompt with 'Not a valid word' if latest row status is 'VALID'"
+    "should not prompt with 'Not a valid word' if latest row status is 'ACCEPTED'"
   );
   it.todo(
     "should prompt with 'Not a valid word' if latest row status is 'INVALID'"
   ); // word length less than 5 or word not in dictionary
 });
+
+const wordLength = 5;
+const guessCount = 6;
+
+const game: WordboardProps["game"] = new Array(guessCount).fill(null).map(() =>
+  new Array(wordLength).fill(null).map(() => ({
+    key: "a",
+    matchStatus: "INITIAL",
+  }))
+);
--- src/components/wordboard/Wordboard.tsx
+++ src/components/wordboard/Wordboard.tsx
@@ -1,1 +1,27 @@
-export const Wordboard = () => null;
+interface Letter {
+  key: KeyCode;
+  matchStatus: MatchStatus;
+}
+
+type Row = Array<Letter>;
+
+export interface WordboardProps {
+  game: Array<Row>;
+  latestRowStatus: "ACCEPTED" | "IN_PROGRESS" | "INVALID";
+}
+
+export const Wordboard = ({ game, latestRowStatus }: WordboardProps) => {
+  return (
+    <section>
+      {game.map((guessWord, wordIdx) => (
+        <div key={wordIdx} aria-label={"guess-word"}>
+          {guessWord.map((letter, letterIdx) => (
+            <span key={letterIdx} aria-label={"letter"}>
+              {letter.matchStatus}
+            </span>
+          ))}
+        </div>
+      ))}
+    </section>
+  );
+};
