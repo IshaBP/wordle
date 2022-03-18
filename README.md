d3153eaa84c55b4e5ecae7a9e84d50b7121b608d
IshaBP <isha.pachkawade11@gmail.com>
2022-03-19 00:38:54 +0530

    test coverage badges

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # wordle
 
 ![Github Workflow Badge](https://github.com/isha-11/wordle/actions/workflows/workflow.yml/badge.svg)
+![Branches coverage](https://raw.githubusercontent.com/isha-11/wordle/badges/badges/badge-branches.svg)
+![Statements coverage](https://raw.githubusercontent.com/isha-11/wordle/badges/badges/badge-statements.svg)
 
 Open source non-profit wordle clone for educational purpose.
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageReporters: ['json-summary', 'lcov', 'text'],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/node_modules/',
@@ -9,10 +10,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      lines: 85,
-      branches: 85,
-      functions: 85,
-      statements: 85,
+      lines: 90,
+      branches: 90,
+      functions: 90,
+      statements: 90,
     },
   },
 };
