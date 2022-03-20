f1edf740a6ff62d2fc7b641b037cc7e6bf2b2b71
IshaBP <isha.pachkawade11@gmail.com>
2022-03-20 18:14:56 +0530

    Web animation API support detection

--- src/components/keyboard/useAnimateKey.ts
+++ src/components/keyboard/useAnimateKey.ts
@@ -3,11 +3,14 @@ import { useTheme } from 'styled-components';
 export const useAnimateKey = () => {
   const theme = useTheme();
   return (keyCode: KeyCode) => {
-    const element: HTMLButtonElement | null = document.querySelector(
-      `[aria-label=keyboard] button[data-code="${keyCode}"]`,
-    );
-    if (element) {
-      element.animate([{ backgroundColor: theme.keyPressBgColor }], 100);
+    if ('animate' in document.body) {
+      // Feature Detection - Web Animation API: Animation will not work if browser does not support Web Animation API
+      const element: HTMLButtonElement | null = document.querySelector(
+        `[aria-label=keyboard] button[data-code="${keyCode}"]`,
+      );
+      if (element) {
+        element.animate([{ backgroundColor: theme.keyPressBgColor }], 100);
+      }
     }
   };
 };
