1ab4fe4b72520ba92887ebe48f7f20fa075970d6
IshaBP <isha.pachkawade11@gmail.com>
2022-03-24 22:46:36 +0530

    configured babel-styled-component

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/wordle/',
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        configFile: true,
+      },
+    }),
+  ],
 });
