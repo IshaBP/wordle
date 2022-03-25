0de2d1899cf083c756dcd240956035d19a07f902
IshaBP <isha.pachkawade11@gmail.com>
2022-03-25 23:57:24 +0530

    useAnimateKey refactor

--- src/components/keyboard/useAnimateKey.ts
+++ src/components/keyboard/useAnimateKey.ts
@@ -1,19 +1,17 @@
 import { useCallback } from 'react';
 import { useTheme } from 'styled-components';
+import { useAnimation } from '../useAnimation';
 
 export const useAnimateKey = () => {
   const theme = useTheme();
+  const animate = useAnimation();
+
   return useCallback(
     (keyCode: KeyCode) => {
-      if ('animate' in document.body) {
-        // Feature Detection - Web Animation API: Animation will not work if browser does not support Web Animation API
-        const element: HTMLButtonElement | null = document.querySelector(
-          `[aria-label=keyboard] button[data-code="${keyCode}"]`,
-        );
-        if (element) {
-          element.animate([{ backgroundColor: theme.bgColor.KEY_PRESS }], 100);
-        }
-      }
+      const keyEl = document.querySelector(
+        `[aria-label=keyboard] button[data-code="${keyCode}"]`,
+      );
+      animate(keyEl, [{ backgroundColor: theme.bgColor.KEY_PRESS }], 100);
     },
     [theme],
   );
