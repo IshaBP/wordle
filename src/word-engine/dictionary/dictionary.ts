c9ff8e1a6bda3daafdd7fd002867d70c74a57449
IshaBP <isha.pachkawade11@gmail.com>
2022-03-29 23:29:06 +0530

    most used words integrated

--- src/word-engine/dictionary/dictionary.ts
+++ src/word-engine/dictionary/dictionary.ts
@@ -1,5 +1,7 @@
+import mostUsedWords from /* preval */ './most-used-words';
 import baseDictionary from /* preval */ './base-dictionary';
 
+console.log(mostUsedWords.length);
 export const isInDictionary = (word: string) => baseDictionary.includes(word);
 
 export const getRandomWord = () => 'angel';
