ac79c077976b3e3d9bea11801e79ef3e2f77a45c
IshaBP <isha.pachkawade11@gmail.com>
2022-04-01 23:47:26 +0530

    useStorageReducer hook in progress

--- src/data-access/useStorageReducer.ts
+++ src/data-access/useStorageReducer.ts
@@ -1,3 +1,16 @@
+import { useMemo } from 'react';
+
+const getSessionData = <T extends object>(storageKey: string): T | null => {
+  const sessionData = sessionStorage.getItem(storageKey);
+
+  if (sessionData) {
+    const data: T = JSON.parse(sessionData);
+    return data;
+  } else {
+    return null;
+  }
+};
+
 const setSessionData = <T extends object>(storageKey: string, data: T) => {
   sessionStorage.setItem(storageKey, JSON.stringify(data));
 };
@@ -7,7 +20,10 @@ export const useStorageReducer = <S extends object, A extends unknown>(
   initialState: S,
   storageKey: string,
 ): [S, (action: A) => void] => {
-  let state = initialState;
+  let state = useMemo(
+    () => getSessionData<S>(storageKey) || initialState,
+    [initialState, storageKey],
+  );
 
   const dispatch = (action: A) => {
     const newState = reducer(state, action);
