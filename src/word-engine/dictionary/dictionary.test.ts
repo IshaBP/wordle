7d0294b80c9f4b18f8350de86dbd0a119abcfadc
IshaBP <isha.pachkawade11@gmail.com>
2022-04-05 22:42:26 +0530

    isInDictionary tests

--- src/word-engine/dictionary/dictionary.test.ts
+++ src/word-engine/dictionary/dictionary.test.ts
@@ -1,6 +1,16 @@
-import { getRandomWord } from './dictionary';
+import { getRandomWord, isInDictionary } from './dictionary';
 
 describe('Dictionary', () => {
+  describe('isInDictionary', () => {
+    it('should return true if word is in the base dictionary', () => {
+      expect(isInDictionary('allow')).toBe(true);
+    });
+
+    it('should return false if word is not in the base dictionary', () => {
+      expect(isInDictionary('uuuuu')).toBe(false);
+    });
+  });
+
   describe('getRandomWord', () => {
     it('should return a 5 letter word', () => {
       const word = getRandomWord();
