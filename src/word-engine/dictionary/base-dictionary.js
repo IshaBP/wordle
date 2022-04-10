31448eee93ad2115bfa3c4320e387bc5d0810b74
IshaBP <isha.pachkawade11@gmail.com>
2022-04-10 15:47:39 +0530

    added most used words to base dictionary

--- src/word-engine/dictionary/base-dictionary.js
+++ src/word-engine/dictionary/base-dictionary.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const wordListPath = require('word-list');
+const mostUsedWords = require('./most-used-words');
 
 // TODO: Tests
 const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
-const baseDictionary = wordArray.filter((word) => word.length === 5);
+const baseDictionary = wordArray
+  .filter((word) => word.length === 5)
+  .concat(mostUsedWords);
 
-module.exports = baseDictionary;
+module.exports = Array.from(new Set(baseDictionary));
